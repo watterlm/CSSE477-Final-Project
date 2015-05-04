aa5a57672c68dfc2968606dda7dd941388d8696b
watterlm <watterlm@rose-hulman.edu>
2015-05-03 23:25:05 -0400

    Revert "Revert "fixed putplugin""
    This reverts commit acc8cbd2e8d6e1fbecc3e5543de3898f3f5c8b41.

--- PUTplugin/src/putplugin/myHandler.java
+++ PUTplugin/src/putplugin/myHandler.java
@@ -7,16 +7,18 @@ import java.io.IOException;
 import protocol.HttpRequest;
 import protocol.HttpResponseFactory;
 import protocol.IHandler;
+import protocol.IHttpRequest;
+import protocol.IHttpResponse;
+import protocol.InternalErrorResponse;
 import protocol.Protocol;
 import protocol.ServletHandlerResponse;
 
 public class myHandler implements IHandler{
 
-	public void handle(HttpRequest request, ServletHandlerResponse servlet){
+	public void handle(IHttpRequest request, ServletHandlerResponse servlet) throws IOException{
 		
-		HttpResponseFactory responseFactory = new HttpResponseFactory(server);
-		response = responseFactory.createResponse(null, Protocol.CLOSE, Protocol.OK_CODE);
+		IHttpResponse response = new InternalErrorResponse();
 		servlet.setResponse(response);
-		servlet.write(response);
+		servlet.write();
 	}
 }
